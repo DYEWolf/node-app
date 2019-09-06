17aab9ff3882e0f3ca1e8d89a9720ef7a17d5865
Christopher Angel <cristopher.sanmina@sanmina.com>
2019-09-06 17:07:43 -0500

    first commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+
+var app = require('../index.js');
+
+describe('GET /', function() {
+    it('respond with hello world', function(done) {
+        request(app).get('/').expect('hello world', done)
+    });
+});
+
